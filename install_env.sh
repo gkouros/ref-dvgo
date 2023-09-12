b8f08a3ed37468c871da90e2cacb0bf465b041c8
George Kouros <george.kouros.ece@gmail.com>
2023-09-12 10:05:41 +0200

    Fix name of conda environment

--- install_env.sh
+++ install_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/miniconda3/etc/profile.d/conda.sh
 conda create -n refdvgo python=3.9 -y && \
-    conda activate temp && \
+    conda activate refdvgo && \
     conda install 'pytorch<2.0.0' torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y && \
     conda install "cuda-nvcc==11.7" "cuda-toolkit==11.7" -c nvidia -c conda-forge
     conda install 'gxx==11.2.0' -c conda-forge -y && \
