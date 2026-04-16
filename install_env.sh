818e1a74b70b1146984d57d1e0f15c34634a20f7
George Kouros <george.kouros.ece@gmail.com>
2026-04-16 17:35:48 +0200

    Fix installation script and requirements

--- install_env.sh
+++ install_env.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
-source ~/miniconda3/etc/profile.d/conda.sh
-conda create -n refdvgo python=3.9 -y && \
-    conda activate refdvgo && \
-    conda install 'pytorch<2.0.0' torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia -y && \
-    conda install "cuda-nvcc==11.7" "cuda-toolkit==11.7" -c nvidia -c conda-forge
-    conda install 'gxx==11.2.0' -c conda-forge -y && \
-    conda install cudatoolkit cudatoolkit-dev  -c conda-forge
-    conda install pytorch-scatter -c pyg -y && \
-    pip3 install -r requirements.txt
+#!/bin/bash -l
+
+source ~/miniforge3/etc/profile.d/conda.sh
+
+mamba create -n refdvgo python=3.9.15 -y && \
+    mamba activate refdvgo && \
+    mamba install 'gxx==11.2.0' -c conda-forge -y && \
+    mamba install 'pytorch<2.0.0' torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia -y && \
+    pip3 install -r requirements.txt && \
+    mamba install -y "mkl=2024.0.0" && \
+    pip install -U openmim && mim install mmcv && \
+    mamba install -c conda-forge binutils && \
+    mamba install pytorch-scatter -c pyg && \
+    mamba install 'gxx==11.2.0' -c conda-forge -y && \
+    pip install torch_efficient_distloss
